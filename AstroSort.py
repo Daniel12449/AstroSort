ef3f7b5c2446b926cb0c7c5f46bbda614d9a5adb
Daniel Schauer <git@dnls.eu>
2024-10-15 20:34:31 +0200

    renamed window to mainwindow

--- AstroSort.py
+++ AstroSort.py
@@ -20,8 +20,8 @@ canceled = False
         
 # -- Start of functions ---
 def querySimbad():
-    window.combo_box_query.clear()
-    queryString = window.line_simbad_query.text()
+    mainWindow.combo_box_query.clear()
+    queryString = mainWindow.line_simbad_query.text()
     
     simbad = Simbad()
     simbad.ROW_LIMIT = 10
@@ -34,29 +34,29 @@ def querySimbad():
                 if "NAME" in name: 
                     name = name.strip("NAME ")
                 coordinates = [simbad_results[i]["RA"], simbad_results[i]["DEC"], name]
-                window.combo_box_query.addItem(name, coordinates)
+                mainWindow.combo_box_query.addItem(name, coordinates)
         except: 
             QtWidgets.QMessageBox.about(None, "Simbad Search", "Simbad was unable to find an object")
             logging.warning("Simbad was unable to find a matching object.")
                     
 def updateCoordinates():
     global object_name
-    window.label_ra_coordinates.setText(window.combo_box_query.currentData()[0])
-    window.label_dec_coordinates.setText(window.combo_box_query.currentData()[1])
-    object_name = window.combo_box_query.currentData()[2].replace(" ", "")
-    logging.info("Object set to " + object_name + " with coordinates: RA " + window.combo_box_query.currentData()[0] + " and DEC " + window.combo_box_query.currentData()[1])
+    mainWindow.label_ra_coordinates.setText(mainWindow.combo_box_query.currentData()[0])
+    mainWindow.label_dec_coordinates.setText(mainWindow.combo_box_query.currentData()[1])
+    object_name = mainWindow.combo_box_query.currentData()[2].replace(" ", "")
+    logging.info("Object set to " + object_name + " with coordinates: RA " + mainWindow.combo_box_query.currentData()[0] + " and DEC " + mainWindow.combo_box_query.currentData()[1])
     
 def updateObjectName(source):
     global object_name
     
     if source == 1:
-        object_name = window.line_object_name1.text()
+        object_name = mainWindow.line_object_name1.text()
     elif source == 2:
-        object_name = window.line_object_name2.text()
+        object_name = mainWindow.line_object_name2.text()
     elif source == 3:
-        object_name = window.line_object_name3.text()
+        object_name = mainWindow.line_object_name3.text()
     elif source == 5:
-        object_name = window.line_object_name4.text()
+        object_name = mainWindow.line_object_name4.text()
         
     logging.info("Object name set to: " + str(object_name))
     
@@ -72,7 +72,7 @@ def updateCategory(source):
     elif source == 3:
         category = "Constellations"
     elif source == 4:
-        category = window.line_category.text()
+        category = mainWindow.line_category.text()
         
     logging.info("Object category set to: " + str(category))
     
@@ -83,76 +83,76 @@ def openFiles(target):
     if target == 'lights':
         for path in path_list:
             light_files.append(path)
-            window.list_lights.addItem(path.name)
+            mainWindow.list_lights.addItem(path.name)
             logging.info("Light file added: " + str(path))
     elif target == 'darks':
         for path in path_list:
             dark_files.append(path)
-            window.list_darks.addItem(path.name)
+            mainWindow.list_darks.addItem(path.name)
             logging.info("Dark file added: " + str(path))
     elif target == 'flats':
         for path in path_list:
             flat_files.append(path)
-            window.list_flats.addItem(path.name)
+            mainWindow.list_flats.addItem(path.name)
             logging.info("Flat file added: " + str(path))
     elif target == 'bias':
         for path in path_list:
             bias_files.append(path)
-            window.list_bias.addItem(path.name)
+            mainWindow.list_bias.addItem(path.name)
             logging.info("Bias file added: " + str(path))
             
 def removeFiles(target):
     if target == 'lights':
-        row = window.list_lights.currentRow()
+        row = mainWindow.list_lights.currentRow()
         logging.info("Removing light file: " + str(light_files[row]))
         light_files.pop(row)
-        window.list_lights.takeItem(row)
+        mainWindow.list_lights.takeItem(row)
         
     elif target == 'darks':
-        row = window.list_darks.currentRow()
+        row = mainWindow.list_darks.currentRow()
         logging.info("Removing dark file: " + str(dark_files[row]))
         dark_files.pop(row)
-        window.list_darks.takeItem(row)
+        mainWindow.list_darks.takeItem(row)
         
     elif target == 'flats':
-        row = window.list_flats.currentRow()
+        row = mainWindow.list_flats.currentRow()
         logging.info("Removing flat file: " + str(flat_files[row]))
         flat_files.pop(row)
-        window.list_flats.takeItem(row)
+        mainWindow.list_flats.takeItem(row)
         
     elif target == 'bias':
-        row = window.list_lights.currentRow()
+        row = mainWindow.list_lights.currentRow()
         logging.info("Removing bias file: " + str(bias_files[row]))
         light_files.pop(row)
-        window.list_lights.takeItem(row)
+        mainWindow.list_lights.takeItem(row)
         
 def updateOutputPath():
     global output_path
     outputPath = QtWidgets.QFileDialog.getExistingDirectory()
-    window.line_output_path.setText(outputPath)
+    mainWindow.line_output_path.setText(outputPath)
     output_path = pathlib.Path(outputPath)
     logging.info("Output path set to: " + str(output_path))
     
 def updateCamera():
     global camera
-    camera = window.line_camera.text()
+    camera = mainWindow.line_camera.text()
     logging.info("Camera set to: " + camera)
     
 def updateFocalLength():
     global focal_length
-    focal_length = window.line_focal_length.text()
+    focal_length = mainWindow.line_focal_length.text()
     logging.info("Focal length set to: " + focal_length)
     
 def updateLocation():
     global location
-    location = window.line_location.text()
+    location = mainWindow.line_location.text()
     logging.info("Location set to: " + location)
     
 def updateDate():
     if light_files != []:
         first_file = light_files[0]
         date = datetime.datetime.fromtimestamp(first_file.stat().st_mtime, tz=datetime.timezone.utc)
-        window.date.setDate(date)
+        mainWindow.date.setDate(date)
         logging.info("Date set to: " + str(date))
         
 def setCancel():
@@ -185,20 +185,20 @@ def copy_and_rename():
         logging.error("Location not set!")
         
     else:
-        date = str(window.date.date().day()) + "-" + str(window.date.date().month()) + "-" + str(window.date.date().year())
+        date = str(mainWindow.date.date().day()) + "-" + str(mainWindow.date.date().month()) + "-" + str(mainWindow.date.date().year())
         output_path_final = output_path / pathlib.Path(category) / pathlib.Path(object_name) / pathlib.Path(date + "_" + location) / pathlib.Path(camera + "_" + focal_length)
         logging.info("Base output path set to: " + str(output_path_final))
         
         # Progress Bar
         total_files = len(light_files) + len(dark_files) + len(flat_files) + len(bias_files)
-        window.progress_bar.setMinimum(0)
-        window.progress_bar.setMaximum(total_files)
+        mainWindow.progress_bar.setMinimum(0)
+        mainWindow.progress_bar.setMaximum(total_files)
         current_file = 0
         
         for file in light_files:
             if canceled: return None
             current_file += 1
-            window.progress_bar.setValue(current_file)
+            mainWindow.progress_bar.setValue(current_file)
             source = file
             destination_path = output_path_final / pathlib.Path('LIGHTS') 
             destination_path.mkdir(parents=True, exist_ok=True)
@@ -211,7 +211,7 @@ def copy_and_rename():
         for file in dark_files:
             if canceled: return None
             current_file += 1
-            window.progress_bar.setValue(current_file)
+            mainWindow.progress_bar.setValue(current_file)
             source = file
             destination_path = output_path_final / pathlib.Path('DARKS') 
             destination_path.mkdir(parents=True, exist_ok=True)
@@ -224,7 +224,7 @@ def copy_and_rename():
         for file in flat_files:
             if canceled: return None
             current_file += 1
-            window.progress_bar.setValue(current_file)
+            mainWindow.progress_bar.setValue(current_file)
             source = file
             destination_path = output_path_final / pathlib.Path('FLATS') 
             destination_path.mkdir(parents=True, exist_ok=True)
@@ -237,7 +237,7 @@ def copy_and_rename():
         for file in bias_files:
             if canceled: return None
             current_file += 1
-            window.progress_bar.setValue(current_file)
+            mainWindow.progress_bar.setValue(current_file)
             source = file
             destination_path = output_path_final / pathlib.Path('BIAS') 
             destination_path.mkdir(parents=True, exist_ok=True)
@@ -249,46 +249,46 @@ def copy_and_rename():
 
 @Slot()      
 def startProcess(self):
-    window.thread_manager.start(copy_and_rename)
+    mainWindow.thread_manager.start(copy_and_rename)
     logging.info("Starting rename and copy process.")
 
 if __name__ == "__main__":    
     app = QtWidgets.QApplication([])
-    window = MainWindow()
-    window.resize(1200, 600)
+    mainWindow = MainWindow()
+    mainWindow.resize(1200, 600)
     
     logging.basicConfig(level=logging.INFO)
     
-    window.combo_box_category.currentIndexChanged.connect(window.displayStack)
-    window.line_object_name1.editingFinished.connect(lambda: updateObjectName(1))
-    window.line_object_name2.editingFinished.connect(lambda: updateObjectName(2))
-    window.line_object_name3.editingFinished.connect(lambda: updateObjectName(3))
-    window.line_object_name4.editingFinished.connect(lambda: updateObjectName(4))
-    window.line_category.editingFinished.connect(lambda: updateCategory(5))
+    mainWindow.combo_box_category.currentIndexChanged.connect(mainWindow.displayStack)
+    mainWindow.line_object_name1.editingFinished.connect(lambda: updateObjectName(1))
+    mainWindow.line_object_name2.editingFinished.connect(lambda: updateObjectName(2))
+    mainWindow.line_object_name3.editingFinished.connect(lambda: updateObjectName(3))
+    mainWindow.line_object_name4.editingFinished.connect(lambda: updateObjectName(4))
+    mainWindow.line_category.editingFinished.connect(lambda: updateCategory(5))
     
-    window.button_query.clicked.connect(querySimbad)
-    window.line_simbad_query.returnPressed.connect(querySimbad)
-    window.combo_box_query.currentIndexChanged.connect(updateCoordinates)
-    window.line_camera.editingFinished.connect(updateCamera)
-    window.line_focal_length.editingFinished.connect(updateFocalLength)
-    window.line_location.editingFinished.connect(updateLocation)
+    mainWindow.button_query.clicked.connect(querySimbad)
+    mainWindow.line_simbad_query.returnPressed.connect(querySimbad)
+    mainWindow.combo_box_query.currentIndexChanged.connect(updateCoordinates)
+    mainWindow.line_camera.editingFinished.connect(updateCamera)
+    mainWindow.line_focal_length.editingFinished.connect(updateFocalLength)
+    mainWindow.line_location.editingFinished.connect(updateLocation)
     
-    window.button_add_lights.clicked.connect(lambda: openFiles('lights'))
-    window.button_add_darks.clicked.connect(lambda: openFiles('darks'))
-    window.button_add_flats.clicked.connect(lambda: openFiles('flats'))
-    window.button_add_bias.clicked.connect(lambda: openFiles('bias'))
+    mainWindow.button_add_lights.clicked.connect(lambda: openFiles('lights'))
+    mainWindow.button_add_darks.clicked.connect(lambda: openFiles('darks'))
+    mainWindow.button_add_flats.clicked.connect(lambda: openFiles('flats'))
+    mainWindow.button_add_bias.clicked.connect(lambda: openFiles('bias'))
     
-    window.button_del_lights.clicked.connect(lambda: removeFiles('lights'))
-    window.button_del_darks.clicked.connect(lambda: removeFiles('darks'))
-    window.button_del_flats.clicked.connect(lambda: removeFiles('flats'))
-    window.button_del_bias.clicked.connect(lambda: removeFiles('bias'))
+    mainWindow.button_del_lights.clicked.connect(lambda: removeFiles('lights'))
+    mainWindow.button_del_darks.clicked.connect(lambda: removeFiles('darks'))
+    mainWindow.button_del_flats.clicked.connect(lambda: removeFiles('flats'))
+    mainWindow.button_del_bias.clicked.connect(lambda: removeFiles('bias'))
     
-    window.button_output_path.clicked.connect(updateOutputPath)
-    window.list_lights.model().rowsInserted.connect(updateDate)
-    window.button_cancel.clicked.connect(setCancel)
-    window.button_start.clicked.connect(startProcess)
+    mainWindow.button_output_path.clicked.connect(updateOutputPath)
+    mainWindow.list_lights.model().rowsInserted.connect(updateDate)
+    mainWindow.button_cancel.clicked.connect(setCancel)
+    mainWindow.button_start.clicked.connect(startProcess)
     
-    window.show()
+    mainWindow.show()
     sys.exit(app.exec())
 
         
