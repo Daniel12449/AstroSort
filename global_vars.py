3cb00826fa52692225cc1f4af7035e026061d67f
Daniel Schauer <git@dnls.eu>
2024-10-24 16:29:10 +0200

    Complete rewrite of the tool.

--- global_vars.py
+++ global_vars.py
@@ -1,4 +1,13 @@
-light_files = []
-dark_files = []
-bias_files = []
-flat_files = []
+output_files_list = {
+    'light_input': [],
+    'dark_input': [],
+    'flat_input': [],
+    'bias_input': [],
+    'light_output': [],
+    'dark_output': [],
+    'flat_output': [],
+    'bias_output': [],
+}
+
+output_final_dir = None
+canceled = False
