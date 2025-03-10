5a632c7f82e8bb72608820adea1131c1cae50eea
Daniel Schauer <git@dnls.eu>
2025-03-10 16:31:35 +0100

    Hopefully fixed crash from updating Process Bar from different thread than gui

--- test_s3.py
+++ test_s3.py
@@ -1,43 +1,50 @@
 import boto3
+import hashlib
 
-# Credentials
-s3_url = 'https://cephgw2.relaix.net'
-s3_access_key = 'D5VC3MNPUMEL6K6HI1RX'
-s3_secret_key = '9DdOSprGHzUmVHzTsGE8aBVL7XnA1jr3Kw8GOe1T'
+#from configparser import ConfigParser
+
+# config_path = pathlib.Path('/home/daniel/.config/astrosort.ini')
+
+# cfg = ConfigParser()
+# cfg.read(config_path)
+       
+
+# service_name = cfg.get('S3', 'service_name', fallback='s3')
+# region_name = cfg.get('S3', 'region_name', fallback=None)
+# api_version = cfg.get('S3', 'api_version', fallback=None)
+# use_ssl = cfg.getboolean('S3', 'use_ssl', fallback='True')
+# verify = cfg.getboolean('S3', 'verify', fallback='True')
+# endpoint_url = cfg.get('S3', 'endpoint_url', fallback=None)
+# aws_access_key_id = cfg.get('S3', 'aws_access_key_id', fallback=None)
+# aws_secret_access_key = cfg.get('S3', 'aws_secret_access_key', fallback=None)
+# aws_session_token = cfg.get('S3', 'aws_session_token', fallback=None)
+# config = cfg.get('S3', 'config', fallback=None)
+
+service_name = 's3'
+endpoint_url = 'https://cephgw2.relaix.net'
+aws_access_key_id = 'D5VC3MNPUMEL6K6HI1RX' 
+aws_secret_access_key = '9DdOSprGHzUmVHzTsGE8aBVL7XnA1jr3Kw8GOe1T'
+
+resource = boto3.resource(service_name=service_name,
+                #region_name=region_name,
+                #api_version=api_version,
+                #use_ssl=use_ssl,
+                #verify=bool(verify),
+                endpoint_url=endpoint_url,
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key,
+                #aws_session_token=aws_session_token,
+                #config=config
+            )
+
+conn = boto3
+
+input_path = 'astrosort.ini'
+new_name = 'File/test.ini'
+bucket_name = 'test-astroraw'
+#resource.put_object(input_path, new_name, Body= ChecksumAlgorithm="sha1")
 
 
+with open(input_path, 'rb') as data:
+    resource.Bucket(bucket_name).put_object(Key=new_name, Body=data)
 
-s3_target = boto3.resource('s3', 
-    endpoint_url=s3_url,
-    aws_access_key_id=s3_access_key,
-    aws_secret_access_key=s3_secret_key,
-    aws_session_token=None,
-    config=boto3.session.Config(signature_version='s3v4')
-)
-
-#https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3/bucket/index.html
-
-#buckets = s3_target.buckets.all()
-#for bucket in buckets:
-#    print(bucket.name)
-    
-int_buckets = s3_target.Bucket('zeitraffer')
-
-folders = set()
-files = []
-
-for obj in int_buckets.objects.all():
-    # Add the folder (prefix) to the set (folder is determined by the object's key)
-    folder = obj.key.split('/')[0]
-    if folder and obj.key != folder:
-        folders.add(folder)
-    # Add the file to the list of files
-    files.append(obj.key)
-    
-print("Folders:")
-for folder in folders:
-    print(f"Folder: {folder}/")
-
-print("\nFiles:")
-for file in files:
-    print(f"File: {file}")
